0c795653de88bdff2bcee19cf2f757b39718b0a7
Cole Smith <smithcole@protonmail.com>
2022-02-03 17:13:07 -0800

    fixed technik-e map

--- keyboards/technik-e/main.py
+++ keyboards/technik-e/main.py
@@ -13,11 +13,10 @@ keyboard.modules = [layers_ext, modtap]
 rgb_ext = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=48)
 keyboard.extensions.append(rgb_ext)
 # keymap
-keyboard.keymap = [[KC.RGB_TOG, KC.A, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.L, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.L, KC.L, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.H, KC.DOT, KC.L, KC.LALT, KC.L, KC.LCTRL, KC.NO, KC.L, KC.SLASH, KC.TAB, KC.RCTRL, KC.NO], 
+keyboard.keymap = [[KC.RGB_TOG, KC.A, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.L, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.L, KC.L, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.H, KC.DOT, KC.L, KC.LALT, KC.L, KC.LCTRL, KC.NO, KC.L, KC.SLASH, KC.TAB, KC.RCTRL], 
 [KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT, KC.QUOTE, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO], 
 [KC.NO, KC.AT, KC.HASH, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.PLUS, KC.NO, KC.NO, KC.PIPE, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO], 
-[KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.F11, KC.F12, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO], 
-[KC.NO]] 
+[KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.F11, KC.F12, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO]] 
 # keymap
 if __name__ == '__main__':
     if supervisor.runtime.usb_connected:
