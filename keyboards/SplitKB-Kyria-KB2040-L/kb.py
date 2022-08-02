dadbe5250227404232a44f0eee2084bb2f3627cf
Jan Lindblom <janlindblom@fastmail.fm>
2022-08-02 22:32:20 +0300

    Adding the SplitKB Kyria Keyboard with KB2040.

---
+++ keyboards/SplitKB-Kyria-KB2040-L/kb.py
@@ -0,0 +1,33 @@
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.scanners import DiodeOrientation
+
+class KMKKeyboard(_KMKKeyboard):
+    col_pins = (
+        board.A3,
+        board.A2,
+        board.A1,
+        board.A0,
+        board.SCK,
+        board.MISO,
+        board.MOSI,
+        board.D10,
+    )
+    row_pins = (board.D8, board.D7, board.D6, board.D4)
+    diode_orientation = DiodeOrientation.COL2ROW
+    data_pin = board.D1
+    rgb_pixel_pin = board.D0
+    SCL = board.D3
+    SDA = board.D2
+    encoder_pin_0 = board.D5
+    encoder_pin_1 = board.D9
+
+    # fmt: off
+    coord_mapping = [
+         0,  1,  2,  3,  4,  5,                     37, 36, 35, 34, 33, 32,
+         8,  9, 10, 11, 12, 13,                     45, 44, 43, 42, 41, 40,
+        16, 17, 18, 19, 20, 21, 22, 23,     55, 54, 53, 52, 51, 50, 49, 48,
+                    27, 28, 29, 30, 31,     63, 62, 61, 60, 59
+    ]
+    # fmt: on
